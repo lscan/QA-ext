58a1cd41a70c89e84cbb3b73276a7a0262bf58a2
Josh Cramoy <jdcramoy@gmail.com>
2015-11-19 15:29:02 -0500

    Create contentscript2.js

---
+++ contentscript2.js
@@ -0,0 +1,55 @@
+
+  var onPageData2 = document.querySelectorAll('#migration-table .url-box:nth-child(3) a');
+  var liveArray = [];
+
+function copyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+
+  // Place in top-left corner of screen regardless of scroll position.
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+
+  // Ensure it has a small width and height. Setting to 1px / 1em
+  // doesn't work as this gives a negative w/h on some browsers.
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+
+  // We don't need padding, reducing the size if it does flash render.
+  textArea.style.padding = 0;
+
+  // Clean up any borders.
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+
+  // Avoid flash of white box if rendered for any reason.
+  textArea.style.background = 'transparent';
+
+  textArea.value = text;
+
+  document.body.appendChild(textArea);
+
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
+  }
+
+  document.body.removeChild(textArea);
+};
+
+
+  for (var i = 0; i < onPageData2.length; i++) {
+    console.log(onPageData2[i].innerHTML);
+    liveArray.push(onPageData2[i]);
+    console.log(liveArray.toString());
+};
+
+copyTextToClipboard(liveArray.join('\n')); 	
+
+
