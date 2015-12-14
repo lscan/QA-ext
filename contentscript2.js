77326f4cf3d1ee5dc4aa2b146a4e716108b6598d
Josh Cramoy <jdcramoy@gmail.com>
2015-12-14 17:59:38 -0500

    Draft mode exclude URL update
    changes to file to exclude URLs that are in draft mode from the QA process

--- contentscript2.js
+++ contentscript2.js
@@ -1,6 +1,8 @@
 
+  //declares the variable for the URL of the migration pages and establishes the array 
   var onPageData2 = document.querySelectorAll('#migration-table .url-box:nth-child(3) a');
   var liveArray = [];
+  
 
 function copyTextToClipboard(text) {
   var textArea = document.createElement("textarea");
@@ -43,13 +45,41 @@ function copyTextToClipboard(text) {
   document.body.removeChild(textArea);
 };
 
+/* For loop cycles through onPageData2[i] as well as cycling through 
+sibling element sibling[2] (which === 'draft-box'). 
+If siblings[2].innerHTML === 'true', then the innerHTML of the onPageData2 iteration is
+pushed to liveArray[];
+*/
+for (var i = 0; i < onPageData2.length; i++) {
+  console.log(onPageData2[i].innerHTML);
+  var getSiblings = function (elem) {
+    var siblings = [];
+    var sibling = elem.parentNode.firstChild;
+     for ( ; sibling; sibling = sibling.nextSibling ) {
+        if ( sibling.nodeType === 1 && sibling !== elem ) {
+                siblings.push( sibling );
+                }
+            }
+            return siblings;
+        };
+    // declares the elem variable as onPageData2[i] and moves the target element to the parent element
+    var elem = onPageData2[i].parentNode;
+    var siblings = getSiblings(elem);
+    
+   //if statement to qualify whether the URL is in draft mode or live. It check the 
+   //inner html of the array item siblings[2] which equates to the .draft-box of the current iteration
+    if (siblings[2].innerHTML === 'false'){
+            //this method pushes the contents of the current onPageData2 iteration to the array
+            liveArray.push(onPageData2[i]);
+            console.log(liveArray.toString());
+              } else {
+                console.log('draft');
+              }
+          }; 
 
-  for (var i = 0; i < onPageData2.length; i++) {
-    console.log(onPageData2[i].innerHTML);
-    liveArray.push(onPageData2[i]);
-    console.log(liveArray.toString());
-};
-
+//This function copies the text to the clipboard and joins the results of the array, 
+// removing commas and adding a return line break        
 copyTextToClipboard(liveArray.join('\n')); 	
+
 
 
