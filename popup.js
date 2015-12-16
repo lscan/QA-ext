983f65617a344901c411a72144e51f4030b83aaa
Josh Cramoy <jdcramoy@gmail.com>
2015-12-16 12:42:46 -0500

    Update to count and pass live and Draft URL Values
    Update to count and pass live and Draft URL Values. Also added "Copied" functionality to buttons.

--- popup.js
+++ popup.js
@@ -1,14 +1,45 @@
+
+//declare variables for Sandbox and Live copy buttons to add event listener later
+//for click
 var copySandboxBtn = document.querySelector('.js-copy-sandbox-btn'),
   copyLiveBtn = document.querySelector('.js-copy-live-btn');
 
-
-
+//declare variables to store draft and live URL count
+var liveUrlCount;
+var draftUrlCount;
+//execute contentscript2.js on button click
 copySandboxBtn.addEventListener('click', function(event) {
   chrome.tabs.executeScript({file:"contentscript2.js"});
-  
 });
 
+//execute contentscript.js on button click
 copyLiveBtn.addEventListener('click', function(event) {
   chrome.tabs.executeScript({file:"contentscript.js"});
   });
+
+//add listener to handle messaging from content scripts
+chrome.runtime.onMessage.addListener(function(request, sender){
+    //grabs the items from the array and assigns them to the variables
+    liveUrlCount = request.message[0];
+    draftUrlCount = request.message[1];
+    msg = request.message[2];
+    //console.log values for QA
+    console.log(request.message[0]);
+    console.log(request.message[1]);
+    console.log(request.message[2]);
+    //traverse DOM of popup.html and add the values to the divs
+    document.getElementById('liveUrlCount').innerHTML = liveUrlCount;
+    document.getElementById('draftUrlCount').innerHTML = draftUrlCount;
+
+    if (msg === "script two") {
+      document.querySelector('.js-copy-sandbox-btn').innerHTML = "Copied!";
+    } else if (msg === "script one") {
+      document.querySelector('.js-copy-live-btn').innerHTML = "Copied!";
+    }
+    else {
+      console.log('copying not successful');
+    }
+});
+
+
 
