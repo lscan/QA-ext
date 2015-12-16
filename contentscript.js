2888a8cb872cfaa60be6f20efaa4771292ce138f
Josh Cramoy <jdcramoy@gmail.com>
2015-12-16 12:39:51 -0500

    Update to count and pass live and Draft URL Values
    Update to count and pass live and Draft URL Values

--- contentscript.js
+++ contentscript.js
@@ -1,11 +1,14 @@
+
 //declares the variable for the URL of the migration pages and establishes the array 
 var onPageData1 = document.querySelectorAll('#migration-table .url-box:nth-child(2) a');
 var sandboxArray = [];
-
-
+var liveUrlCount = 0; 
+var draftUrlCount = 0;
+var scriptMsg ="script one";
 
 function copyTextToClipboard(text) {
   var textArea = document.createElement("textarea");
+  var msg;
 
   // Place in top-left corner of screen regardless of scroll position.
   textArea.style.position = 'fixed';
@@ -52,6 +55,7 @@ pushed to liveArray[];
 */
 for (var i = 0; i < onPageData1.length; i++) {
         console.log(onPageData1[i].innerHTML);
+        // This function is essentially the .siblings() method from jQuery using vanilla js
         var getSiblings = function (elem) {
             var siblings = [];
             var sibling = elem.parentNode.firstChild;
@@ -63,21 +67,37 @@ for (var i = 0; i < onPageData1.length; i++) {
             return siblings;
         };
 
-        // declares the elem variable as onPageData2[i] and moves the target element to the parent element
+        // declares the elem variable as onPageData2[i] and moves the target element up a level 
+        // in the DOM to the parent element
         var elem = onPageData1[i].parentNode;
         var siblings = getSiblings(elem);
     //if statement to qualify whether the URL is in draft mode or live. It check the 
-   //inner html of the array item siblings[2] which equates to the .draft-box of the current iteration
+    //inner html of the array item siblings[2] which equates to the .draft-box of the current iteration
     if (siblings[2].innerHTML === 'false'){
             //this method pushes the contents of the current onPageData2 iteration to the array
             sandboxArray.push(onPageData1[i]);
             console.log(sandboxArray.toString());
+            liveUrlCount++;
+            console.log(liveUrlCount);
               } else {
                 console.log('draft');
+                draftUrlCount++;
               }
           }; 
 //This function copies the text to the clipboard and joins the results of the array, 
 // removing commas and adding a return line break       
 copyTextToClipboard(sandboxArray.join('\n'));
+
+//put liveUrlCount and draftUrlCount into an array so that we can get the values
+// in the popup.js file using message[0] and message[1]
+var urlArray = [liveUrlCount, draftUrlCount, scriptMsg];
+
+//pass urlArray through chrome messaging to send to popup.js
+chrome.runtime.sendMessage({message: urlArray}, function(response){
+    console.log(response);
+    });
+
+
+
 
 
